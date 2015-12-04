016bea1c463dd185d20f50e64dde01f57dc468c2
Lee Zhen Yong <bruceoutdoors@gmail.com>
2015-12-04 17:35:56 +0800

    cleanups

--- src/core/DB.java
+++ src/core/DB.java
@@ -1,9 +1,5 @@
 package core;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -28,10 +24,6 @@ public class DB {
         migrateDb();
 
         mConnection = DriverManager.getConnection(DB_URL);
-    }
-
-    public DB(Connection mConnection) {
-        this.mConnection = mConnection;
     }
 
     public static DB getInstance() {
